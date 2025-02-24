f3c6b2b34d3eb0bc1e2f4d3bc0533319ea93bf6e
HwangSettong <iamsehyun@gmail.com>
2025-02-24 13:54:13 +0900

    first commit

---
+++ src/main/java/com/jasoseohelper/JasoseoHelperApplication.java
@@ -0,0 +1,13 @@
+package com.jasoseohelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasoseoHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasoseoHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasoseohelper/JasoseoHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasoseohelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasoseoHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
