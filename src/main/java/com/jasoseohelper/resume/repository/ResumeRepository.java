d37a2c3a862704f5de306255d6b9992dffeb935a
HwangSettong <iamsehyun@gmail.com>
2025-03-15 03:09:49 +0900

    Chore: 사용 안하는 메서드 삭제

--- src/main/java/com/jasoseohelper/resume/repository/ResumeRepository.java
+++ src/main/java/com/jasoseohelper/resume/repository/ResumeRepository.java
@@ -3,8 +3,4 @@ package com.jasoseohelper.resume.repository;
 import com.jasoseohelper.resume.entity.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-    Optional<Resume> findByRidAndUid(Long rid, Long uid);
-}
+public interface ResumeRepository extends JpaRepository<Resume, Long> { }
