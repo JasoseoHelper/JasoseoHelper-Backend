b7b4faaa1a1011d221798ccc27ca5c8846b5cfd2
neu99 <neu990112@gmail.com>
2025-04-08 08:30:41 +0900

    feat: question

--- src/main/java/com/jasoseohelper/question/dto/QuestionResponseDTO.java
+++ src/main/java/com/jasoseohelper/question/dto/QuestionResponseDTO.java
@@ -1,13 +1,20 @@
 package com.jasoseohelper.question.dto;
 
+import java.sql.Timestamp;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class QuestionResponseDTO {
     private Long qid;
     private String title;
+    private String guide;
+    private Timestamp c_date;
+    private String content;
+    private String feedback;
 }
